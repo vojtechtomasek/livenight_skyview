3c3d942a92747f97bc894ef422c377046b2dba03
vojtechtomasek <vojtatomasek2007@gmail.com>
2025-11-16 21:06:52 +0100

    Enhance UI with Liquid Glass effects in Object Detail and Sky View Bottom Bar

--- lib/screens/object_detail/object_detail_screen.dart
+++ lib/screens/object_detail/object_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 import 'widgets/object_header.dart';
 import 'widgets/object_basic_info.dart';
 import 'widgets/object_description.dart';
@@ -21,7 +22,6 @@ Future<void> showObjectDetailSheet(BuildContext context, {String? objectName}) {
   );
 }
 
-
 class _ObjectDetailDraggableSheet extends StatelessWidget {
   final String? objectName;
   const _ObjectDetailDraggableSheet({this.objectName});
@@ -33,47 +33,74 @@ class _ObjectDetailDraggableSheet extends StatelessWidget {
       minChildSize: 0.25,
       maxChildSize: 0.9,
       builder: (context, controller) {
-        return Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Color(0xFF0A0F2C), Color(0xFF0E1B47)],
-            ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 12, bottom: 6),
-                child: Container(
-                  width: 44,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: CupertinoColors.white.withValues(alpha: 0.3),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
+        return Stack(
+          children: [
+            LiquidGlassLayer(
+              settings: const LiquidGlassSettings(
+                thickness: 25,
+                blur: 20,
+                glassColor: Color(0x400A0F2C),
+                refractiveIndex: 1.5,
+                lightIntensity: 0.8,
+              ),
+              child: LiquidGlass(
+                shape: const LiquidRoundedSuperellipse(
+                  borderRadius: 32,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12, bottom: 6),
+                      child: Container(
+                        width: 44,
+                        height: 5,
+                        decoration: BoxDecoration(
+                          color: CupertinoColors.white.withValues(alpha: 0.5),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        controller: controller,
+                        padding: const EdgeInsets.fromLTRB(20, 4, 20, 32),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 20),
+                            ObjectHeader(),
+                            SizedBox(height: 24),
+                            ObjectBasicInfo(),
+                            SizedBox(height: 24),
+                            ObjectDescription(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Expanded(
-                child: SingleChildScrollView(
-                  controller: controller,
-                  padding: const EdgeInsets.fromLTRB(20, 4, 20, 32),
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 20),
-                      ObjectHeader(),
-                      SizedBox(height: 24),
-                      ObjectBasicInfo(),
-                      SizedBox(height: 24),
-                      ObjectDescription(),
+            ),
+            
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              height: 50,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0x000A0F2C),
+                      Color(0xFF0A0F2C),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         );
       },
     );
--- lib/screens/sky_view/widgets/sky_view_bottom_bar.dart
+++ lib/screens/sky_view/widgets/sky_view_bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 import 'package:provider/provider.dart';
 import '../../../providers/conpass_provider.dart';
 import 'compass_widget.dart';
@@ -13,19 +14,38 @@ class SkyViewBottomBar extends StatelessWidget {
       right: 0,
       child: SafeArea(
         top: false,
-        child: Container(
-          height: 64,
-          color: CupertinoColors.black.withValues(alpha: .30),
-          child: Consumer<CompassProvider>(
-            builder: (_, compass, __) {
-              final heading = compass.heading ?? 0.0;
-              return CompassWidget(
-                heading: heading,
-                fovDegrees: 120,
-                color: CupertinoColors.white,
-                markerColor: const Color(0xFF8AB4F8),
-              );
-            },
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 12),
+          child: LiquidGlass.withOwnLayer(
+            settings: const LiquidGlassSettings(
+              blur: 18.0,
+              thickness: 15.0,
+              refractiveIndex: 1.45,
+              
+              glassColor: Color(0x4410162D),
+                
+              lightIntensity: 1.3,
+              ambientStrength: 0.35,
+              
+              saturation: 1.05,
+            ),
+            shape: const LiquidRoundedSuperellipse(
+              borderRadius: 26,
+            ),
+            child: SizedBox(
+              height: 64,
+              child: Consumer<CompassProvider>(
+                builder: (_, compass, __) {
+                  final heading = compass.heading ?? 0.0;
+                  return CompassWidget(
+                    heading: heading,
+                    fovDegrees: 120,
+                    color: CupertinoColors.white,
+                    markerColor: const Color(0xFF8AB4F8),
+                  );
+                },
+              ),
+            ),
           ),
         ),
       ),
